70af5ebc83aa06e4445ff870ad953eb0136812a7
SluggDev <100637730+Lalimace420@users.noreply.github.com>
2024-06-25 00:35:21 -0400

    Update main.lua

--- slugg_headlight/client/main.lua
+++ slugg_headlight/client/main.lua
@@ -353,19 +353,3 @@ AddEventHandler('openmenuheadlight2', function()
     exports['dopeNotify2']:Alert("Erreur", "Tu n'est pas dans un vehicule!", 5000, 'error')
   end
 end)
-
-
-Citizen.CreateThread(function()
-  while true do
-      Citizen.Wait(0)
-      local veh = GetVehiclePedIsIn(PlayerPedId(), false)
-      if DoesEntityExist(veh) and not IsEntityDead(veh) then
-          local model = GetEntityModel(veh)
-          -- If it's not a boat, plane or helicopter, and the vehilce is off the ground with ALL wheels, then block steering/leaning left/right/up/down.
-          if not IsThisModelABoat(model) and not IsThisModelAHeli(model) and not IsThisModelAPlane(model) and IsEntityInAir(veh) then
-              DisableControlAction(0, 59) -- leaning left/right
-              DisableControlAction(0, 60) -- leaning up/down
-          end
-      end
-  end
-end)
